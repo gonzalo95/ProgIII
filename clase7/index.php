ed07a06903676ef9e85e94c9db23b9d2fd6a3db8
gonzalo <gonzalojgreco@gmail.com>
2019-10-22 22:02:57 -0300

    clase 7 middleware

--- clase7/index.php
+++ clase7/index.php
@@ -10,45 +10,48 @@
 
     $app = new \Slim\App(["settings" => $config]);
 
-    // $jwt = JWT::encode($token, $key);
-    // $decoded = JWT::decode($jwt, $key, array('HS256'));
-
     $app -> group('/', function()
     {
         $this -> get('main', function (Request $request, Response $response){
+            $response -> getBody() -> write("Ya estas autenticado");
+            return $response;
+        });
+    })->add(function($request, $response, $next){
+        if (isset($request->getHeader('token')[0])) {
             $token = $request->getHeader('token')[0];
-            if ($token) {
-                try {
-                    $decoded = JWT::decode($token, 'clave', array('HS256'));
-                    var_dump($decoded);
-                    $response -> getBody() -> write("ok!!"."<hr>");
-                } catch (Exception $e) {
-                    $response -> getBody() -> write("acceso invalido: ".$e);
-                }
+            try {
+                $decoded = JWT::decode($token, 'clave', array('HS256'));
+                //var_dump($decoded);
+                $response = $next($request, $response);
+            } catch (Exception $e) {
+                $response -> getBody() -> write("acceso invalido: ".$e);
             }
-            return $response;
-        });
+        }
+        else {
+            $response -> getBody() -> write("Usted no se ha logeado");
+        }
+        return $response;
+    });
 
-        $this -> post('login[/]', function (Request $request, Response $response){
-            $args = $request -> getParsedBody();
-            $user = $args['user'];
-            $pass = $args['pass'];
-            if ($user != 'gonzalo' || $pass != '1234') {
-                $response -> getBody() -> write("Acceso denegado");
-            }
-            else {
-                $key = "clave";
-                $token = array(
-                    "saludo" => "hola mundo",
-                    "user" => $user,
-                    "pass" => $pass,
-                    "exp" => time() + 120
-                );
-                $jwt = JWT::encode($token, $key);
-                $response -> getBody() -> write($jwt);
-            }
-            return $response;
-        });
+    $app->post('/login[/]', function (Request $request, Response $response){
+        $args = $request -> getParsedBody();
+        $user = $args['user'];
+        $pass = $args['pass'];
+        if ($user != 'gonzalo' || $pass != '1234') {
+            $response -> getBody() -> write("Acceso denegado");
+        }
+        else {
+            $key = "clave";
+            $token = array(
+                "saludo" => "hola mundo",
+                "user" => $user,
+                "pass" => $pass,
+                "exp" => time() + 120
+            );
+            $jwt = JWT::encode($token, $key);
+            $response -> getBody() -> write($jwt);
+        }
+        return $response;
     });
 
     $app->run();
